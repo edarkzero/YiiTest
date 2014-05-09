5ae38b55223255040c4ed712e5db2c82d1612f46
Edgar Cardona <edgarcardona87@gmail.com>
2014-05-09 13:16:02 -0430

    toHtmlSelectOptions con options custom y all option added #1

--- protected/include/helpers/DataParser.php
+++ protected/include/helpers/DataParser.php
@@ -1,87 +1,89 @@
 <?php
-class DataParser
-{
-	public static function toHtmlSelectOptions($model, $id, $name, $valueAttr, $contenAttr, $multiple = false, $addAllOption = false, $customOptions = NULL)
+
+	class DataParser
 	{
-		$htmlOutput = '<select ';
-		if ($multiple)
-			$htmlOutput .= 'multiple';
-		$htmlOutput .= ' id="' . $id . '" name="' . $name . '">';
-
-		if (isset($model))
+		public static function toHtmlSelectOptions($model, $id, $name, $valueAttr, $contenAttr, $multiple = false, $addAllOption = false, $customOptions = NULL)
 		{
-			if ($addAllOption)
-				$htmlOutput .= '<option value="-1">' . Yii::t('app', 'Select all') . '</option>';
+			$htmlOutput = '<select ';
+			if ($multiple)
+				$htmlOutput .= 'multiple';
+			$htmlOutput .= ' id="' . $id . '" name="' . $name . '">';
 
-			if(isset($customOptions) && is_array($customOptions))
+			if (isset($model))
 			{
-				foreach($customOptions as $option)
+				if ($addAllOption)
+					$htmlOutput .= '<option value="-1">' . Yii::t('app', 'Select all') . '</option>';
+
+				if (isset($customOptions) && is_array($customOptions))
 				{
-					$htmlOutput .= '<option value="' . $option['value'] . '">' . $option['text'] . '</option>';
+					foreach ($customOptions as $option)
+					{
+						$htmlOutput .= '<option value="' . $option['value'] . '">' . $option['text'] . '</option>';
+					}
 				}
+
+				if (is_array($model))
+				{
+					foreach ($model as $data)
+					{
+						$htmlOutput .= '<option value="' . $data->{$valueAttr} . '">' . $data->{$contenAttr} . '</option>';
+					}
+				}
+				else
+					$htmlOutput .= '<option value="' . $model->{$valueAttr} . '">' . $model->{$contenAttr} . '</option>';
+
 			}
 
-			if (is_array($model))
-			{
-				foreach ($model as $data)
-				{
-					$htmlOutput .= '<option value="' . $data->{$valueAttr} . '">' . $data->{$contenAttr} . '</option>';
-				}
-			}
 			else
-				$htmlOutput .= '<option value="' . $model->{$valueAttr} . '">' . $model->{$contenAttr} . '</option>';
+				$htmlOutput .= '<option>' . Yii::t('app', 'No results found') . '</option>';
 
+			$htmlOutput .= '</select>';
+
+			return $htmlOutput;
 		}
 
-		else
-			$htmlOutput .= '<option>' . Yii::t('app', 'No results found') . '</option>';
+		/**
+		 * Convierte la data de un modelo dentro de un array a un array clave valor compatible con listas CHtml
+		 *
+		 * @param $modelData
+		 * @param $fields array[]
+		 * @param $addAllOption string
+		 * @return array
+		 */
+		public static function toDataList($modelData, $fields, $addAllOption = NULL)
+		{
+			if (!isset($modelData))
+				return NULL;
 
-		$htmlOutput .= '</select>';
+			$parsedData = array();
 
-		return $htmlOutput;
+			if ($addAllOption === 'prepend')
+			{
+				$parsedData[-1] = Yii::t('app', 'All');
+			}
+
+			foreach ($modelData as $data)
+			{
+				$string = "";
+
+				foreach ($fields as $key => $field)
+				{
+					if ($key > 0)
+						$string .= ' - ';
+
+					$string .= $data->{$field};
+				}
+
+				$parsedData[$data->id] = $string;
+
+			}
+
+			if ($addAllOption === 'append')
+			{
+				$parsedData[-1] = Yii::t('app', 'All');
+			}
+
+			return $parsedData;
+
+		}
 	}
-
-    /**
-     * Convierte la data de un modelo dentro de un array a un array clave valor compatible con listas CHtml
-     *
-     * @param $modelData
-     * @param $fields array[]
-     * @param $addAllOption string
-     * @return array
-     */
-    public static function toDataList($modelData, $fields, $addAllOption = NULL)
-    {
-        if(!isset($modelData)) return null;
-
-        $parsedData = array();
-
-        if ($addAllOption === 'prepend')
-        {
-            $parsedData[-1] = Yii::t('app', 'All');
-        }
-
-        foreach ($modelData as $data)
-        {
-            $string = "";
-
-            foreach ($fields as $key => $field)
-            {
-                if ($key > 0)
-                    $string .= ' - ';
-
-                $string .= $data->{$field};
-            }
-
-            $parsedData[$data->id] = $string;
-
-        }
-
-        if ($addAllOption === 'append')
-        {
-            $parsedData[-1] = Yii::t('app', 'All');
-        }
-
-        return $parsedData;
-
-    }
-}
