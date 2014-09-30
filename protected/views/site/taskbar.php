fe6573f88b22d5c375e39cb8804ad60fd0ecdc55
Edgar Cardona <edgarcardona87@gmail.com>
2014-09-30 16:14:27 -0430

    taskbar inicio con diseño #2

--- protected/views/site/taskbar.php
+++ protected/views/site/taskbar.php
@@ -4,19 +4,23 @@
 ?>
 
 <div id="taskbar">
-	<div class="taskbar-icon">
-		<a class="taskbar-link" href="#"><?php echo Yii::t('app', 'Test {n}', array('{n}' => 1)); ?></a>
-	</div>
+	<div id="taskbar-content">
+		<div class="taskbar-icon float-left">
+			<a class="taskbar-link" href="#"><?php echo Yii::t('app', 'Test {n}', array('{n}' => 1)); ?></a>
+		</div>
 
-	<div class="taskbar-icon">
-		<a class="taskbar-link" href="#"><?php echo Yii::t('app', 'Test {n}', array('{n}' => 2)); ?></a>
-	</div>
+		<div class="taskbar-icon float-left">
+			<a class="taskbar-link" href="#"><?php echo Yii::t('app', 'Test {n}', array('{n}' => 2)); ?></a>
+		</div>
 
-	<div class="taskbar-icon">
-		<a class="taskbar-link" href="#"><?php echo Yii::t('app', 'Test {n}', array('{n}' => 3)); ?></a>
-	</div>
+		<div class="taskbar-icon float-left">
+			<a class="taskbar-link" href="#"><?php echo Yii::t('app', 'Test {n}', array('{n}' => 3)); ?></a>
+		</div>
 
-	<div class="taskbar-icon">
-		<a class="taskbar-link" href="#"><?php echo Yii::t('app', 'Test {n}', array('{n}' => 4)); ?></a>
+		<div class="taskbar-icon float-left">
+			<a class="taskbar-link" href="#"><?php echo Yii::t('app', 'Test {n}', array('{n}' => 4)); ?></a>
+		</div>
+
+		<div class="clear"></div>
 	</div>
 </div>
